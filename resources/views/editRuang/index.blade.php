b40c67a61513eb0740bc8d1902a3e92d7cff4249
Naufal Farras <godonmiaw@gmail.com>
2023-04-10 21:59:35 +0700

    logout
    iyah

---
+++ resources/views/editRuang/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.master')
+
+@section('content')
+    <h1>Edit Ruang Parkir</h1>
+    @if(session('sukses'))
+        <div class="alert alert-success" role="alert">
+        {{session('sukses')}}
+        </div>
+    @endif
+        <div class="row">
+        <div class="col lg-12">
+        <form action="/pruang1/{{$data_parkir->id}}/update" method="POST">
+            {{csrf_field()}}
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Plat Nomor</label>
+                    <input type="text" name="platNomor" class="form-control" id="exampleInputEmail1" aria-describedby
+                    ="emailHelp" placeholder="{{$data_parkir->platNomor}}" value="{{$data_parkir->platNomor}}" readonly>
+                </div>
+                
+                <div class="form-group">
+                <label for="exampleFormControlSelect1">Plat Nomor yang tersedia</label>
+                <!-- <select class="form-control" name="ruang" id="exampleFormControlSelect1">
+                    @foreach ($data_ruang as $ruang)
+                    <option value="{{$data_parkir->ruangParkir}}">{{$ruang->ruang}}</option>
+                    @endforeach
+                </select> -->
+                <input type="text" name="ruangParkir" class="form-control" id="exampleInputEmail1" aria-describedby
+                    ="emailHelp" placeholder="Ruang" value="{{$data_parkir->ruangParkir}}">
+                </div>
+                <div class="form-group" hidden>
+                <label for="exampleFormControlSelect1">Plat Nomor yang tersedia</label>
+                <select class="form-control" name="sudah_masuk" id="exampleFormControlSelect1">
+                    
+                    <option value="1">1</option>
+                    
+                </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Update</button>
+        </form>
+        </div>
+        </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+ 
+ //
+ Route::get('/home', function(){
+     return view('Home');
+ });
+
+ Route::get('/about', function(){
+     return view('about');
+ });
+
+Route::get('/login', '\App\Http\Controllers\loginController@index')->name('login')->middleware('guest');
+Route::post('/login', '\App\Http\Controllers\loginController@authenticate');
+Route::post('/logout', '\App\Http\Controllers\loginController@logout');
+
+Route::get('/dashboard', function() {
+    return view('dashboard.index');
+})->middLeware('auth');
+ 
+Route::get('/pegawai', '\App\Http\Controllers\AdminController@index'); // Menampilkan data yang ada di database
+Route::post('/pegawai/create', '\App\Http\Controllers\AdminController@create'); // Membuat data baru
+Route::get('/pegawai/{id}/edit', '\App\Http\Controllers\AdminController@edit'); // Mengedit data yang sudah ada
+Route::post('/pegawai/{id}/update', '\App\Http\Controllers\AdminController@update'); // Mengupdate data yang sudah diedit
+Route::get('/pegawai/{id}/delete', '\App\Http\Controllers\AdminController@delete'); // Menghapus data
+
+Route::get('/mall1', function() {
+    return view('mall1.index');
+});
+
+// Petugas Masuk Mall 1
+Route::get('/pmasuk1', '\App\Http\Controllers\pmasuk1Controller@index');
+Route::post('/pmasuk1/add', '\App\Http\Controllers\pmasuk1Controller@add');
+
+// Petugas Ruang Mall 1
+Route::get('/pruang1', '\App\Http\Controllers\pruang1Controller@index');
+Route::get('/pruang1/{id}/edit', '\App\Http\Controllers\pruang1Controller@edit');
+Route::post('/pruang1/{id}/update', '\App\Http\Controllers\pruang1Controller@update');
+
+// Petugas Keluar Mall 1
+Route::get('/pkeluar1', '\App\Http\Controllers\pkeluar1Controller@index');
+Route::get('/pkeluar1/{id}/edit', '\App\Http\Controllers\pkeluar1Controller@edit');
+Route::post('/pkeluar1/{id}/update', '\App\Http\Controllers\pkeluar1Controller@update');
+
+Route::get('/mall2', function() {
+    return view('mall2.index');
+});
+Route::get('/mall3', function() {
+    return view('mall3.index');
+});
